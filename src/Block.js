45c2d74c4ddb9c24d1c0fe6cc259f82bb4f8a059
Peng Zhang <zhangp1@duq.edu>
2021-06-20 23:22:47 +0100

    Add a function to render each block after a certain period of delay when rolling

--- src/Block.js
+++ src/Block.js
@@ -3,12 +3,17 @@ import './Block.css'
 
 class Block extends Component {
     render (){
-        // Use Font Awesome lib to create icon with i tag
-        // String templating
         return (
             <div>
-            {/* <i className={`Block fad fa-${this.props.symbol}`} /> */}
-            <img src={`images/${this.props.symbol}.svg`} className='Block Block-symbol' alt={this.props.symbol}/>
+                {/* Display the symbol for each block based on the rolling status */}
+                { (this.props.rolling) ? (
+                    <img src='images/fruits.svg' className='Block Block-symbol' alt='fruits'/> 
+                    ) : (
+                    <img src={`images/${this.props.symbol}.svg`} className='Block Block-symbol' alt={this.props.symbol}/> 
+                    )
+                }
+                {/* Use Font Awesome lib to create icon with i tag}*/}
+                {/* <i className={`Block fad fa-${this.props.symbol}`} /> */}
             </div>
         )
     }
--- src/PlayJackpot.js
+++ src/PlayJackpot.js
@@ -2,6 +2,8 @@ import react, {Component} from 'react';
 import Block from './Block.js';
 import './PlayJackpot.css'
 
+/* Look into sessionStorage */
+
 class PlayJackpot extends Component {
     //Define the default immutable props - four block symbols
     static defaultProps = {
@@ -23,10 +25,9 @@ class PlayJackpot extends Component {
         */
         this.state = {
             blocks: [
-                {id:1, symbol: 'cherry'}, 
-                {id:2, symbol: 'lemon'}, 
-                {id:3, symbol: 'orange'},
-                // {id:4, symbol: 'watermelon'}
+                {id:1, symbol: 'cherry', rolling: false}, 
+                {id:2, symbol: 'lemon', rolling: false}, 
+                {id:3, symbol: 'orange', rolling: false},
             ], 
             isRolling: false,
             balance: 10
@@ -39,61 +40,58 @@ class PlayJackpot extends Component {
         this.updateBlocks = this.updateBlocks.bind(this);
     }
 
-    //Connect to backend logic to query and update Bank Total
+    //Ajax: Connect to backend logic to query and update Bank Total
     componentDidMount(){
         //load session data, and set state data
 
     }
-
     //Define a roll method to change Block state
     roll(){
-        //Get the random symbol for each block
-        const newBlocks = this.state.blocks.map(block => {
-            //Make a copy of the block object with symbol -> new random symbol
-            return {...block, symbol: this.randSymbol()};
-        });
-
-        //Testing...
-        console.log(newBlocks);
-
-        //Adjust the blocks by updating the state vars to the new array
-        this.setState({
-            blocks: newBlocks, 
-            isRolling: true
-        });
-        //this.updateBlocks(newBlocks); 
+        //Generate random symbol for all blocks
+        this.updateBlocks();
+        
+        //Update global rolling status
+        this.setState({isRolling: true});
 
         //Adjust the session credit
         this.setState(this.updateCredit);
 
-        //Reset rolling state to false
+        //Resetting rolling state to false after the last block stops rolling
         setTimeout(() => {
             this.setState({isRolling: false}) 
-        }, 1000);
+        }, this.state.blocks.length * 1000);
         
     }
+    //Define a function to update Blocks progressively
+    updateBlocks(){
+        //Update rolling status for all blocks to true
+        const newBlocks = this.state.blocks.map(block => {
+            //Make a copy of the block object with symbol -> new random symbol
+            return {...block, rolling: true};
+        });
+        this.setState({blocks: newBlocks});
 
+        //Loop through blocks to create random symbol and update state accordingly
+        //Blocks are updated at 1 sec, 2 sec and 3 sec consecutively
+        for (let i = 0 ; i < this.state.blocks.length; i++){
+            setTimeout(() => {
+                //console.log("before change: " + this.state.blocks[i].symbol);
+                //1. Make a shallow copy of the blocks
+                let blocks = [...this.state.blocks];
+                //2. Make a shallow copy of the current block, and replace the value for "symbol" key 
+                let block = {...blocks[i], symbol : this.randSymbol(), rolling : false}; 
+                //3. Move the array element back to "blocks" array
+                blocks[i] = block;
+                //4. Set the state to the new copy
+                this.setState({blocks});  
+                //console.log("after change: " + this.state.blocks[i].symbol);
+            }, (i + 1) * 1000);
+       }
+            
+    }
     //Define a helper function to get the symbol randomly
     randSymbol(){
         return this.props.symbols[Math.floor(Math.random() * this.props.symbols.length)]['name'];
-    }
-      
-    //Define a helper function to determine if it is a win or a lose
-    updateBlocks(newBlocks){
-        //Set up a timer for each block
-        let timer = 0;
-        newBlocks.map((block, index) =>{            
-            timer += 1000;
-            //console.log(timer);
-            setTimeout(() => {
-              console.log(block); 
-              console.log(this.state.blocks[index]);
-              //this.setState({blocks: newBlocks});
-              this.setState(curState => ({
-                  //blocks[index]: curState.blocks
-              }))
-            }, timer);
-        });
     }
 
     //Define a updateCredit method to adjust session credit after each roll
@@ -126,13 +124,11 @@ class PlayJackpot extends Component {
                 </div>
                 <div className='PlayJackpot-Blocks'>
                     {/* Render all blocks */}
-                    <table>
-                    <tr>
-                    {this.state.blocks.map ( b => 
-                        <td><Block {...b} key={b.id} rolling={this.state.isRolling} /></td>
-                    )}
-                    </tr>
-                    </table>
+                    <table><tbody><tr>
+                        {this.state.blocks.map ( b => 
+                            <td><Block {...b} key={b.id} /></td>
+                        )}
+                    </tr></tbody></table>
                 </div>
                 <div className='PlayJackpot-btns'>
                     <button onClick={this.roll} disabled={this.state.isRolling}>
